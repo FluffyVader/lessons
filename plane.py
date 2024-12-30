27899ec22227f6a490099c5f46171bcb9f9490bd
FluffyVader <wewilllearnthis@gmail.com>
2024-12-30 17:26:44 +0000

    Enemy can now move.

--- plane.py
+++ plane.py
@@ -15,7 +15,8 @@ current_plane_velocity = 0
 
 enemy_planeX = 0
 enemy_planeY = 0
-
+enemy_default_plane_velocity = 2
+enemy_current_plane_velocity = enemy_default_plane_velocity
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -42,20 +43,28 @@ while running:
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_a or event.key == pygame.K_d:
                 current_plane_velocity = 0
-       
+
+
+
         elif event.type == pygame.QUIT:
             running = False
 
     # print(f"default_plane_velocity is {default_plane_velocity} current_plane_velocity is {current_plane_velocity}") debug featers
     
     planeX += current_plane_velocity
-
+    enemy_planeX += enemy_current_plane_velocity
     # Constain X coordinate
     if planeX <= 0:
         planeX = 0
     elif planeX >= WIDTH - planeSpriteWidth:
         planeX = WIDTH - planeSpriteWidth
 
+    if enemy_planeX <= 0:
+        enemy_planeX = 0
+        enemy_current_plane_velocity = -enemy_current_plane_velocity
+    elif enemy_planeX >= WIDTH - enemy_plane_Sprite_Width:
+        enemy_planeX = WIDTH - enemy_plane_Sprite_Width
+        enemy_current_plane_velocity = -enemy_current_plane_velocity
 
     screen.fill("black")
     
