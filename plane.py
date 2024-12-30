04d30bc29e599e1ec7b7e469cd54d898c20fbb1b
FluffyVader <wewilllearnthis@gmail.com>
2024-12-30 16:15:33 +0000

    Added enemy plane

--- plane.py
+++ plane.py
@@ -6,19 +6,30 @@ HEIGHT=500
 WIDTH=400
 FPS=50
 
+#courdinates, velocity and name
+running = True
+planeX = 0
+planeY = 400
+default_plane_velocity = 2  # const Please never modify it in runtime.
+current_plane_velocity = 0
+
+enemy_planeX = 0
+enemy_planeY = 0
+
+
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 pygame.display.set_caption("Plane shooter")
+
+#my plane loading
 planeSprite = pygame.image.load("plane.png")
 planeSpriteWidth = planeSprite.get_width()
 
-
-running = True
-planeX = 0
-planeY = 400
-default_plane_velocity = 2 # const. Please never modify it in runtime.
-current_plane_velocity = 0
+#enemy loading
+enemy_plane_sprite = pygame.image.load("plane.png")
+enemy_plane_Sprite_Width = enemy_plane_sprite.get_width()
+enemy_plane_sprite = pygame.transform.rotate(enemy_plane_sprite, 180)
 
 #Main Loop
 while running:
@@ -26,14 +37,17 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_a:
                 current_plane_velocity = -default_plane_velocity
-                pass
             elif event.key == pygame.K_d:
                 current_plane_velocity = default_plane_velocity
-                pass
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_a or event.key == pygame.K_d:
+                current_plane_velocity = 0
+       
         elif event.type == pygame.QUIT:
             running = False
 
-    print(f"default_plane_velocity is {default_plane_velocity} current_plane_velocity is {current_plane_velocity}")
+    # print(f"default_plane_velocity is {default_plane_velocity} current_plane_velocity is {current_plane_velocity}") debug featers
+    
     planeX += current_plane_velocity
 
     # Constain X coordinate
@@ -41,11 +55,12 @@ while running:
         planeX = 0
     elif planeX >= WIDTH - planeSpriteWidth:
         planeX = WIDTH - planeSpriteWidth
-    
 
 
     screen.fill("black")
+    
     screen.blit(planeSprite,(planeX,planeY))
-
+    screen.blit(enemy_plane_sprite,(enemy_planeX,enemy_planeY))
+    
     clock.tick(FPS)
     pygame.display.flip()
