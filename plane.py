57b6cab45a87b70ab519bb2fc91dfd1285e86971
FluffyVader <wewilllearnthis@gmail.com>
2025-01-07 23:06:03 +0000

    Now the enemy plane will disappear if a bullet hits it.

--- plane.py
+++ plane.py
@@ -31,15 +31,20 @@ planeSpriteWidth = planeSprite.get_width()
 enemy_plane_sprite = pygame.image.load("plane.png")
 enemy_plane_Sprite_Width = enemy_plane_sprite.get_width()
 enemy_plane_sprite = pygame.transform.rotate(enemy_plane_sprite, 180)
+enemy_plane_sprite_rect = enemy_plane_sprite.get_rect() ###############
+enemy_plane_sprite_dead = False
 
 #bullet load
-bullet = pygame.image.load("bullet.png")
+bullet_sprite = pygame.image.load("bullet.png")
 bullet_velocity = 20
+bullet_srite_rect = bullet_sprite.get_rect()
 
 bulletX = 0
-bulletY = 0
+bulletY = planeY - 32
+bullet_srite_rect.top = bulletY
+bullet_srite_rect.left = planeX + (36 - 16)
 
-bullet_state = True
+is_bullet_spawned = False
 #Main Loop
 while running:
     for event in pygame.event.get():
@@ -49,19 +54,21 @@ while running:
             elif event.key == pygame.K_d:
                 current_plane_velocity = default_plane_velocity
             elif event.key == pygame.K_SPACE:
-                bullet_state = False
+                is_bullet_spawned = True
                 bulletX = planeX + (36 - 16)
                 bulletY = planeY - 32
+                bullet_srite_rect.left = planeX + (36 - 16)
+                bullet_srite_rect.top = bulletY
+
         
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_a or event.key == pygame.K_d:
                 current_plane_velocity = 0
 
 
-
-
         elif event.type == pygame.QUIT:
             running = False
+
 
     # print(f"default_plane_velocity is {default_plane_velocity} current_plane_velocity is {current_plane_velocity}") debug featers
     
@@ -81,13 +88,26 @@ while running:
         enemy_planeX = WIDTH - enemy_plane_Sprite_Width
         enemy_current_plane_velocity = -enemy_current_plane_velocity
 
+    enemy_plane_sprite_rect.left = enemy_planeX
+
     screen.fill("black")
     
     screen.blit(planeSprite,(planeX,planeY))
-    screen.blit(enemy_plane_sprite,(enemy_planeX,enemy_planeY))
+    if not enemy_plane_sprite_dead:
+        screen.blit(enemy_plane_sprite,(enemy_planeX,enemy_planeY))
     
-    if not bullet_state:
-        screen.blit(bullet,(bulletX, bulletY))
+    if is_bullet_spawned:
         bulletY -= bullet_velocity
+        screen.blit(bullet_sprite,(bulletX, bulletY))
+        bullet_srite_rect.top = bulletY
+
+
+    if pygame.Rect.colliderect(bullet_srite_rect, enemy_plane_sprite_rect) :
+        print(f"Collided bullet_srite_rect: {bullet_srite_rect}  enemy_plane_sprite_rect{enemy_plane_sprite_rect}")
+        enemy_plane_sprite_dead = True
+
+    #print(enemy_plane_sprite_rect)
+    #print(bullet_srite_rect)
+
     clock.tick(FPS)
     pygame.display.flip()
